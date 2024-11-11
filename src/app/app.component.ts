6e5a5edb307b82fc4e2838a7fa8ee42cb425f6ab
Szymon <szmsmi@gmail.com>
2024-11-11 12:17:51 +0100

    cleanup + automated game loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   subject = new FormControl(0);
-  smartShips = new FormControl(false);
-  delay = new FormControl(1000);
+  automatedLoops = new FormControl(0);
+  smartShips = new FormControl(true);
+  delay = new FormControl(1);
   configuration = new FormControl(0);
   sizeOptions: any = [
     { id: 0, size: 10 },
@@ -21,26 +22,29 @@ export class AppComponent implements OnInit {
     { id: 6, size: 100 },
   ];
   delayOptions: number[] = [1, 10, 100, 1000, 10000];
-  players: string[] = ["RED", "BLU"]; //mozna zrobic formularz pod to i sobie dopisywac
+  players: string[] = ['Czerwony', 'Niebieski'];
   tiles1: number[][] = []; // red
   tiles2: number[][] = []; // blu
   states: any = ['', '💣', '⬛', '💥'];
   color: string[] = ['rgba(255, 0, 0, 0.05)', 'rgba(0, 0, 255, 0.05)'];
   selectedSize: number = 0;
-  selectedDelay: number = 1000;
+  selectedDelay: number = 1;
   shots1: Set<string> = new Set();
   shots2: Set<string> = new Set();
-  queue1: { row: number, col: number }[] = [];
-  queue2: { row: number, col: number }[] = [];
+  queue1: { row: number; col: number }[] = [];
+  queue2: { row: number; col: number }[] = [];
   isStarted: boolean = false;
   shipsSet: boolean = false;
   shipConfigurations: any[] = [];
   shipsToPlace: number[] = [];
-  playerShots: number[] = [0, 0]; // Ardo statystyk
+  playerShots: number[] = [0, 0]; 
   playerHits: number[] = [0, 0];
   isGameOver: boolean = false;
   startTime: number | null = null;
-  gameLogs: string = "Wygrany;Procent trafien 1;Procent trafien2;Nietrafione statki;Czas gry(s);Plansza;Opoznienie;Konfiguracja statkow;\n";
+  shotsToFirstHit: number = 0;
+  firstHitOccurred: boolean = false;
+  gameLogs: string =
+    'Wygrany;Inteligentne statki;Procent trafien 1;Procent trafien 2;Nietrafione statki;Czas gry(s);Wielkosc planszy;Opoznienie;Konfiguracja statkow;Ilosc strzalow do pierwszego trafienia;\n';
   constructor() {}
 
   ngOnInit(): void {
@@ -55,7 +59,7 @@ export class AppComponent implements OnInit {
     });
     this.configuration.valueChanges.subscribe((res3: any) => {
       this.shipsToPlace = [];
-      const selectedConfig = this.shipConfigurations.find(x => x.id == res3);
+      const selectedConfig = this.shipConfigurations.find((x) => x.id == res3);
       if (selectedConfig) {
         this.shipsToPlace = selectedConfig.values;
       }
@@ -81,7 +85,6 @@ export class AppComponent implements OnInit {
     }
     return columnLetter;
   }
-  
 
   createEmptyTilesArray(size: number): number[][] {
     return Array.from({ length: size }, () => Array(size).fill(0));
@@ -93,10 +96,8 @@ export class AppComponent implements OnInit {
   }
 
   start() {
-    let size = this.selectedSize;
     let delay = this.selectedDelay;
     this.isStarted = !this.isStarted;
-
     if (this.isStarted) {
       this.startTime = Date.now();
       this.startShootingLoop(delay);
@@ -106,7 +107,9 @@ export class AppComponent implements OnInit {
   }
 
   setShips() {
-    const selectedConfig = this.shipConfigurations.find(x => x.id == this.configuration.value);
+    const selectedConfig = this.shipConfigurations.find(
+      (x) => x.id == this.configuration.value
+    );
     if (selectedConfig) {
       this.shipsToPlace = selectedConfig.values;
     }
@@ -120,23 +123,22 @@ export class AppComponent implements OnInit {
     let rowIndex: number;
     let colIndex: number;
     let shotKey: string;
-    let availableShots = size * size - (playerIndex === 0 ? this.shots1.size : this.shots2.size);
+    let availableShots =
+      size * size - (playerIndex === 0 ? this.shots1.size : this.shots2.size);
 
-    // Check if there are free fields available
     if (availableShots <= 0) {
       this.isStarted = false;
       return;
     }
-    const hasPlayer1Ships = this.tiles1.some(row => row.includes(2)); // check statkow
-    const hasPlayer2Ships = this.tiles2.some(row => row.includes(2));
+    const hasPlayer1Ships = this.tiles1.some((row) => row.includes(2)); // check statkow
+    const hasPlayer2Ships = this.tiles2.some((row) => row.includes(2));
 
-    // Check game over conditions
     if (!hasPlayer1Ships) {
       this.isGameEnded(2);
       return;
     }
     if (!hasPlayer2Ships) {
-      this.isGameEnded(1); //
+      this.isGameEnded(1);
       return;
     }
 
@@ -149,14 +151,18 @@ export class AppComponent implements OnInit {
       (playerIndex === 1 && this.shots2.has(shotKey))
     );
 
-    // Count the shot for the current player
     this.playerShots[playerIndex]++;
+
+    if (this.firstHitOccurred == null) {
+      this.shotsToFirstHit++;
+    }
 
     if (playerIndex === 0) {
       this.shots1.add(shotKey);
       if (this.tiles2[rowIndex][colIndex] === 2) {
         this.tiles2[rowIndex][colIndex] = 3;
         this.playerHits[0]++;
+        this.firstHitOccurred = true;
         console.log(`[1] HIT ${rowIndex}:${this.getColumnLetter(colIndex)}`);
       } else {
         this.tiles2[rowIndex][colIndex] = 1;
@@ -167,6 +173,7 @@ export class AppComponent implements OnInit {
       if (this.tiles1[rowIndex][colIndex] === 2) {
         this.tiles1[rowIndex][colIndex] = 3;
         this.playerHits[1]++;
+        this.firstHitOccurred = true;
         console.log(`[2] HIT ${rowIndex}:${this.getColumnLetter(colIndex)}`);
       } else {
         this.tiles1[rowIndex][colIndex] = 1;
@@ -178,7 +185,9 @@ export class AppComponent implements OnInit {
   private startShootingLoop(delay: number) {
     const shootPlayers = (playerIndex: number) => {
       if (!this.isStarted) return;
-      this.smartShips.value == false ? this.shoot(playerIndex) : this.shootSmart(playerIndex);
+      this.smartShips.value == false
+        ? this.shoot(playerIndex)
+        : this.shootSmart(playerIndex);
       setTimeout(() => {
         shootPlayers(playerIndex === 0 ? 1 : 0);
       }, delay);
@@ -187,17 +196,31 @@ export class AppComponent implements OnInit {
   }
 
   placeShipsOnBoard(tiles: number[][], ships: number[]) {
-    ships.forEach(shipLength => {
+    ships.forEach((shipLength) => {
       let placed = false;
 
       while (!placed) {
         const isVertical = Math.random() < 0.5; // kierunek
-        const rowIndex = Math.floor(Math.random() * (isVertical ? this.selectedSize - shipLength + 1 : this.selectedSize));
-        const colIndex = Math.floor(Math.random() * (isVertical ? this.selectedSize : this.selectedSize - shipLength + 1));
+        const rowIndex = Math.floor(
+          Math.random() *
+            (isVertical
+              ? this.selectedSize - shipLength + 1
+              : this.selectedSize)
+        );
+        const colIndex = Math.floor(
+          Math.random() *
+            (isVertical
+              ? this.selectedSize
+              : this.selectedSize - shipLength + 1)
+        );
 
-        if (this.canPlaceShip(tiles, rowIndex, colIndex, shipLength, isVertical)) {
+        if (
+          this.canPlaceShip(tiles, rowIndex, colIndex, shipLength, isVertical)
+        ) {
           for (let i = 0; i < shipLength; i++) {
-            tiles[isVertical ? rowIndex + i : rowIndex][isVertical ? colIndex : colIndex + i] = 2; //set ship 
+            tiles[isVertical ? rowIndex + i : rowIndex][
+              isVertical ? colIndex : colIndex + i
+            ] = 2; //set ship
           }
           placed = true;
         }
@@ -205,21 +228,35 @@ export class AppComponent implements OnInit {
     });
   }
 
-  canPlaceShip(tiles: number[][], rowIndex: number, colIndex: number, shipLength: number, isVertical: boolean): boolean {
+  canPlaceShip(
+    tiles: number[][],
+    rowIndex: number,
+    colIndex: number,
+    shipLength: number,
+    isVertical: boolean
+  ): boolean {
     for (let i = 0; i < shipLength; i++) {
       const row = isVertical ? rowIndex + i : rowIndex;
       const col = isVertical ? colIndex : colIndex + i;
 
-      if (row >= this.selectedSize || col >= this.selectedSize || tiles[row][col] === 2) {
+      if (
+        row >= this.selectedSize ||
+        col >= this.selectedSize ||
+        tiles[row][col] === 2
+      ) {
         return false; // Cannot place the ship
       }
     }
 
-    // all directions
     const directions = [
-      [-1, -1], [-1, 0], [-1, 1],
-      [0, -1],           [0, 1],
-      [1, -1], [1, 0], [1, 1]
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, -1],
+      [0, 1],
+      [1, -1],
+      [1, 0],
+      [1, 1],
     ];
 
     for (let i = 0; i < shipLength; i++) {
@@ -229,7 +266,12 @@ export class AppComponent implements OnInit {
       for (const [dx, dy] of directions) {
         const adjRow = row + dx;
         const adjCol = col + dy;
-        if (adjRow >= 0 && adjRow < this.selectedSize && adjCol >= 0 && adjCol < this.selectedSize) {
+        if (
+          adjRow >= 0 &&
+          adjRow < this.selectedSize &&
+          adjCol >= 0 &&
+          adjCol < this.selectedSize
+        ) {
           if (tiles[adjRow][adjCol] === 2) {
             return false;
           }
@@ -247,18 +289,61 @@ export class AppComponent implements OnInit {
     const endTime = Date.now();
     const durationInSeconds = (endTime - (this.startTime ?? endTime)) / 1000;
 
-    console.log(`Gra zakończona! Wygrał gracz ${this.players[winningPlayer - 1]}!`);
+    console.log(
+      `Gra zakończona! Wygrał gracz ${this.players[winningPlayer - 1]}!`
+    );
     console.log(`Czas gry: ${durationInSeconds.toFixed(2)} sekund.`);
-    
-    const hitPercentage1 = this.playerShots[0] > 0 ? (this.playerHits[0] / this.playerShots[0]) * 100 : 0;
-    const hitPercentage2 = this.playerShots[1] > 0 ? (this.playerHits[1] / this.playerShots[1]) * 100 : 0;
-    const remainingShips = winningPlayer === 1 ? this.tiles1.flat().filter(tile => tile === 2).length : this.tiles2.flat().filter(tile => tile === 2).length;
 
-    console.log(`Gracz 1 wykonał ${this.playerShots[0]} strzałów, z czego ${this.playerHits[0]} były celne (${hitPercentage1.toFixed(4)}%).`);
-    console.log(`Gracz 2 wykonał ${this.playerShots[1]} strzałów, z czego ${this.playerHits[1]} były celne (${hitPercentage2.toFixed(4)}%).`);
-    console.log(`Gratulacje! Gracz ${this.players[winningPlayer - 1]} ma ${remainingShips} statków pozostałych.`);
-    let endGameString = `${winningPlayer};${hitPercentage1.toFixed(4)};${hitPercentage2.toFixed(4)};${remainingShips};${durationInSeconds.toFixed(2)};${this.selectedSize};${this.selectedDelay};[${this.shipsToPlace}];\n`;
+    const hitPercentage1 =
+      this.playerShots[0] > 0
+        ? (this.playerHits[0] / this.playerShots[0]) * 100
+        : 0;
+    const hitPercentage2 =
+      this.playerShots[1] > 0
+        ? (this.playerHits[1] / this.playerShots[1]) * 100
+        : 0;
+    const remainingShips =
+      winningPlayer === 1
+        ? this.tiles1.flat().filter((tile) => tile === 2).length
+        : this.tiles2.flat().filter((tile) => tile === 2).length;
+
+    console.log(
+      `Gracz 1 wykonał ${this.playerShots[0]} strzałów, z czego ${
+        this.playerHits[0]
+      } były celne (${hitPercentage1.toFixed(4)}%).`
+    );
+    console.log(
+      `Gracz 2 wykonał ${this.playerShots[1]} strzałów, z czego ${
+        this.playerHits[1]
+      } były celne (${hitPercentage2.toFixed(4)}%).`
+    );
+    let inteligent = this.smartShips.value == true ? 'true' : 'false';
+    let endGameString = `${this.players[winningPlayer - 1]};${inteligent};${hitPercentage1.toFixed(
+      4
+    )};${hitPercentage2.toFixed(
+      4
+    )};${remainingShips};${durationInSeconds.toFixed(2)};${
+      this.selectedSize
+    };${this.selectedDelay};[${this.shipsToPlace}];${this.shotsToFirstHit};\n`;
     this.gameLogs += endGameString;
+    if(this.automatedLoops.value > 0){
+      let value = this.automatedLoops.value - 1;
+      this.automatedLoops.patchValue(value);
+      setTimeout(()=>{
+        this.firstHitOccurred = false;
+        this.isGameOver = false;
+        this.shipsSet = false;
+        this.shots1 = new Set();
+        this.shots2 = new Set();
+        this.playerShots = [0, 0]; 
+        this.playerHits = [0, 0];
+        this.generateTiles();
+        this.setShips();
+        setTimeout(()=>{
+          this.start();
+        }, 100)
+      }, 500)
+    }
   }
 
   resetGame() {
@@ -277,7 +362,7 @@ export class AppComponent implements OnInit {
     const now = new Date();
     const dateString = now.toISOString().slice(0, 10);
     const timeString = now.toTimeString().slice(0, 5).replace(':', '-');
-    const filename = `${dateString}_${timeString}_SHIPS.csv`;
+    const filename = `${dateString}_${timeString}_SHIPS.txt`;
 
     const a = document.createElement('a');
     a.href = url;
@@ -291,45 +376,45 @@ export class AppComponent implements OnInit {
   generateShipConfigurations(size: number) {
     this.shipConfigurations = [];
     let configs = [];
-    
+
     if (size <= 10) {
       configs = [
-        { id: 0, values: [3, 2, 2, 1, 1, 1] }, 
+        { id: 0, values: [3, 2, 2, 1, 1, 1] },
         { id: 1, values: [2, 2, 1, 1, 1, 1] },
         { id: 2, values: [3, 3, 2, 1, 1, 1, 1] },
         { id: 3, values: [2, 2, 2, 2, 1, 1, 1] },
-        { id: 4, values: [8] }, 
+        { id: 4, values: [8] },
       ];
     } else if (size <= 20) {
       configs = [
         { id: 0, values: [4, 3, 3, 2, 2, 1, 1, 1, 1] },
         { id: 1, values: [3, 3, 3, 2, 2, 1, 1, 1, 1] },
         { id: 2, values: [4, 4, 3, 3, 2, 2, 1, 1, 1, 1] },
-        { id: 3, values: [3, 3, 3, 2, 2, 2, 1, 1, 1, 1] }
+        { id: 3, values: [3, 3, 3, 2, 2, 2, 1, 1, 1, 1] },
       ];
     } else if (size <= 30) {
       configs = [
         { id: 0, values: [5, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1, 1] },
         { id: 1, values: [4, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1, 1] },
         { id: 2, values: [5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 1, 1, 1] },
-        { id: 3, values: [5, 4, 4, 4, 3, 3, 3, 2, 2, 1, 1, 1, 1] }
+        { id: 3, values: [5, 4, 4, 4, 3, 3, 3, 2, 2, 1, 1, 1, 1] },
       ];
     } else if (size <= 50) {
       configs = [
         { id: 0, values: [6, 5, 5, 4, 4, 3, 3, 3, 2, 2, 1, 1, 1, 1, 1] },
         { id: 1, values: [6, 5, 4, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1] },
         { id: 2, values: [7, 6, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 1, 1, 1] },
-        { id: 3, values: [6, 6, 5, 5, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1, 1] }
+        { id: 3, values: [6, 6, 5, 5, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1, 1] },
       ];
     } else {
       configs = [
         { id: 0, values: [8, 7, 7, 6, 6, 5, 4, 4, 3, 3, 3, 2, 2, 1, 1, 1, 1] },
         { id: 1, values: [9, 8, 7, 6, 6, 5, 5, 4, 3, 3, 3, 2, 2, 1, 1, 1, 1] },
         { id: 2, values: [8, 8, 7, 6, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 1, 1, 1] },
-        { id: 3, values: [9, 7, 7, 6, 6, 5, 5, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1] }
+        { id: 3, values: [9, 7, 7, 6, 6, 5, 5, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1] },
       ];
     }
-    
+
     this.shipConfigurations = configs;
   }
   shootSmart(playerIndex: number) {
@@ -337,23 +422,23 @@ export class AppComponent implements OnInit {
     let rowIndex: number;
     let colIndex: number;
     let shotKey: string = '';
-    let availableShots = size * size - (playerIndex === 0 ? this.shots1.size : this.shots2.size);
-  
+    let availableShots =
+      size * size - (playerIndex === 0 ? this.shots1.size : this.shots2.size);
+
     if (availableShots <= 0) {
       this.isStarted = false;
       return;
     }
-  
-    const hasPlayer1Ships = this.tiles1.some(row => row.includes(2)); // check statkow
-    const hasPlayer2Ships = this.tiles2.some(row => row.includes(2));
-  
-    // Check game over conditions
+
+    const hasPlayer1Ships = this.tiles1.some((row) => row.includes(2)); // check statkow
+    const hasPlayer2Ships = this.tiles2.some((row) => row.includes(2));
+
     if (!hasPlayer1Ships) {
       this.isGameEnded(2);
       return;
     }
     if (!hasPlayer2Ships) {
-      this.isGameEnded(1); 
+      this.isGameEnded(1);
       return;
     }
 
@@ -369,7 +454,7 @@ export class AppComponent implements OnInit {
       do {
         rowIndex = Math.floor(Math.random() * size);
         colIndex = Math.floor(Math.random() * size);
-        shotKey = `${rowIndex}:${colIndex}`; 
+        shotKey = `${rowIndex}:${colIndex}`;
       } while (
         (playerIndex === 0 && this.shots1.has(shotKey)) ||
         (playerIndex === 1 && this.shots2.has(shotKey))
@@ -377,12 +462,17 @@ export class AppComponent implements OnInit {
     }
 
     this.playerShots[playerIndex]++;
-  
+
+    if (!this.firstHitOccurred) {
+      this.shotsToFirstHit++;
+    }
+
     if (playerIndex === 0) {
       this.shots1.add(shotKey);
       if (this.tiles2[rowIndex][colIndex] === 2) {
         this.tiles2[rowIndex][colIndex] = 3;
         this.playerHits[0]++;
+        this.firstHitOccurred = true;
         console.log(`[1] HIT ${rowIndex}:${this.getColumnLetter(colIndex)}`);
         this.queueAdjacentCells(0, rowIndex, colIndex);
       } else {
@@ -394,6 +484,7 @@ export class AppComponent implements OnInit {
       if (this.tiles1[rowIndex][colIndex] === 2) {
         this.tiles1[rowIndex][colIndex] = 3;
         this.playerHits[1]++;
+        this.firstHitOccurred = true;
         console.log(`[2] HIT ${rowIndex}:${this.getColumnLetter(colIndex)}`);
         this.queueAdjacentCells(1, rowIndex, colIndex);
       } else {
@@ -402,22 +493,31 @@ export class AppComponent implements OnInit {
       }
     }
   }
-  
-  private queueAdjacentCells(playerIndex: number, rowIndex: number, colIndex: number) {
+
+  private queueAdjacentCells(
+    playerIndex: number,
+    rowIndex: number,
+    colIndex: number
+  ) {
     const directions = [
       { row: -1, col: 0 }, // up
-      { row: 1, col: 0 },   // down
-      { row: 0, col: -1 },  // left
-      { row: 0, col: 1 },  // right
+      { row: 1, col: 0 }, // down
+      { row: 0, col: -1 }, // left
+      { row: 0, col: 1 }, // right
     ];
-  
+
     const tiles = playerIndex === 0 ? this.tiles2 : this.tiles1;
-  
+
     directions.forEach(({ row, col }) => {
       const newRow = rowIndex + row;
       const newCol = colIndex + col;
-  
-      if (newRow >= 0 && newRow < this.selectedSize && newCol >= 0 && newCol < this.selectedSize) {
+
+      if (
+        newRow >= 0 &&
+        newRow < this.selectedSize &&
+        newCol >= 0 &&
+        newCol < this.selectedSize
+      ) {
         if (tiles[newRow][newCol] !== 1 && tiles[newRow][newCol] !== 3) {
           if (playerIndex === 0) {
             this.queue1.push({ row: newRow, col: newCol });
@@ -427,8 +527,5 @@ export class AppComponent implements OnInit {
         }
       }
     });
-  
   }
-  
-
 }
